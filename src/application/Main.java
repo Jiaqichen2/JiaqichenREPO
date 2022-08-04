5a23c17db402943a8cb9e5c4bff72d35d7fd51f7
elray <elray@LAPTOP-JTBQNPR2>
2022-08-05 01:20:56 +0200

    Added my branch Label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Zeyad's Label"));
+			root.getChildren().add(new Label("Zeyad's Branch"));
 			Scene scene = new Scene(root,400,400);
 			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
